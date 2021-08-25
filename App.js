7498913d543abafc87332ec7042f8b07b5e6fb2b
DanielCastillo <castillouparela10@gmail.com>
2021-08-25 12:16:03 -0500

    load 1216

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tu Profe 24/7</Text>
+      <Text>Tu Profe 24/7_Daniel</Text>
       <StatusBar style="auto" />
     </View>
   );
