c1de09b4058b5c0a989289e31539c82bee3f1592
Daniel Castillo <castillouparela10@gmail.com>
2021-08-26 01:13:59 -0500

    versión 7 s Please enter the commit message for your changes. Lines starting

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'; 
+
+  const Stack = createNativeStackNavigator();
+
+  import UserList from './screen/UserList';
+  import CreateUserScreen from './screen/CreateUserScreen';
+  import UserDetailScreen from './screen/UserDetailScreen';
+
+function MyStack () {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name = "UserList" component = {UserList}/>
+      <Stack.Screen name = "CreateUserScreen" component = {CreateUserScreen}/>
+      <Stack.Screen name = "UserDetailScreen" component = {UserDetailScreen}/>
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Tu Profe 24/7_Daniel</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
 
