a8ed6215827ec1554e715c445b49ba986d6d68f1
Daniel Castillo <castillouparela10@gmail.com>
2021-08-27 10:35:05 -0500

    Versión 8_DB-Firebase

---
+++ database/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCeu0Iw0mgBnkUuc3B86JCgYnv6zFZoAMg",
+  authDomain: "bdtuprofe24-7.firebaseapp.com",
+  projectId: "bdtuprofe24-7",
+  storageBucket: "bdtuprofe24-7.appspot.com",
+  messagingSenderId: "526479134362",
+  appId: "1:526479134362:web:07fbce512d15ccca415923"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export default{
+    firebase,
+    db,
+};
--- screen/CreateUserScreen.js
+++ screen/CreateUserScreen.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Button, TextInput, ScrollView, StyleSheet } from 'react-native';
+import firebase from '../database/firebase'
 
 const CreateUserScreen = () => {
+
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        phone: "",
+    });
+
+    const handleChangeText = (name, value) => {
+        setState({...state, [name]: value });
+    };
+
+    const saveNewUser = async () => {
+        if(state.name == '' || state.email == '' || state.phone == ''){
+            alert('Por favor, digite todas las entradas solicitadas')
+        }else {
+            await firebase.db.collection('users').add({
+                name: state.name,
+                email: state.email,
+                phone: state.phone,
+            })
+            //console.log(state) //muestra en consola el dato almacenado
+            alert('Saved')
+        }
+    }
+
+    /*function storeHighScore() {
+        firebase
+          .database()
+          .ref('users/' + state.name)
+    }*/
+
     return (
         <ScrollView style ={styles.container}>
 
             <View style = {styles.inputGroup}>
-                <TextInput placeholder="Name User"/>
+                <TextInput 
+                    placeholder="Name User"
+                    onChangeText={(value) => handleChangeText('name', value)}
+                />
             </View>
 
             <View style = {styles.inputGroup}>
-                <TextInput placeholder="Email User"/>
+                <TextInput 
+                    placeholder="Email User"
+                    onChangeText={(value) => handleChangeText('email', value)}
+                />
             </View>
 
             <View style = {styles.inputGroup}>
-                <TextInput placeholder="Phone User"/>
+                <TextInput 
+                    placeholder="Phone User"
+                    onChangeText={(value) => handleChangeText('phone', value)}
+                />
             </View>
 
             <View>
-                <Button title="Save User"/>
+                <Button title="Save User" onPress={() => saveNewUser()}/>
             </View>
         </ScrollView>
     )
